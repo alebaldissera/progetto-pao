71bb36a3d8ac3ddfbe1528f610cf539fc803828e
nicolasalberti00 <75423237+nicolasalberti00@users.noreply.github.com>
2021-01-10 11:23:47 +0100

    Aggiunti metodi in Catalogo.cpp
    Metodi getSize e getFileCount. Aggiunte idee per la move (da verificare), snippet codice per la add (per ora ancora non modificato) e costruttore di default (da verificare)

--- Katalog/Catalogo.cpp
+++ Katalog/Catalogo.cpp
@@ -1,6 +1,41 @@
 #include "Catalogo.h"
+#include "BaseNode.h"
 
-Catalogo::Catalogo()
+using namespace Katalog;
+using namespace std;
+
+Catalogo::Catalogo() : root(nullptr) {} //non sono sicuro
+
+/*
+void Catalogo::add(string path, BaseNode* destination_dir)
 {
+    std::regex rgx("(?=\/)(.*?)(?<=\/)");
+        std::smatch match;
+        if(std::regex_search(s.begin(), s.end(), match, rgx)){
+            std::cout<< match[1] << endl; //stamperà: miacartella
+            path = path.substring(match[1].end(), path.end()); //path = "/miofile.jpg"
+        }
+        return 0;
+}
+*/
+
+void Catalogo::move(string path_start, string path_end)
+{
+    /*   idea1: controllo se le due stringhe esistono: sostituisco a path_end la parte finale, così che possa puntare al file in path_start.
+     *          Successivamente elimino dal path start il nome del file appena spostato.
+     *   idea2: controllo se esistono le due stringhe scorrendo con un for il catalogo: successivamente copio il file nel path di destinazione,
+     *          ne modifico il path facendolo combaciare con la destinazione, elimino il file dal path iniziale.
+     */
 
 }
+
+long Catalogo::getSize()
+{
+   return root->getAllSize();
+}
+
+int Catalogo::getFileCount()
+{
+    return root->getFilesCount();
+}
+
--- Katalog/Catalogo.h
+++ Katalog/Catalogo.h
@@ -1,41 +1,47 @@
 #ifndef CATALOGO_H
 #define CATALOGO_H
 #include "BaseNode.h"
+#include <regex>
+#include <iostream>
+
+namespace Katalog{
 
 class Catalogo
 {
 private:
-    //devo definire il contenitore root
+    BaseNodePtr root;
 public:
     /**
-     * @brief add: aggiunge un file alla directory scelta
+     * @brief   add: aggiunge un file alla directory scelta
      * @param   string: è il path del file da inserire
      * @param   BaseNode*: è la directory di destinazione in cui si aggiunge il file
      */
     void add(std::string, BaseNode*);
     /**
-     * @brief remove: rimuove un file dalla sua posizione
-     * @return directory da cui è stato eliminato il file
+     * @brief   remove: rimuove un file dalla sua posizione
+     * @return  directory da cui è stato eliminato il file
      * @param   string: path del file da rimuovere
      */
     BaseNode* remove(std::string);
     /**
-     * @brief move: permette di spostare un file da una directory all'altra
+     * @brief   move: permette di spostare un file da una directory all'altra
      * @param   string: path dell'oggetto da spostare
      * @param   string: path della destinazione in cui spostare l'oggetto
      */
     void move(string, string);
     /**
-     * @brief getSize: ritorna la dimensione del file/directory
-     * @return long
+     * @brief   getSize: ritorna la dimensione del file/directory
+     * @return  long
      */
     long getSize();
     /**
-     * @brief getFileCount: ritorna il numero di file presenti nella directory
-     * @return int
+     * @brief   getFileCount: ritorna il numero di file presenti nella directory
+     * @return  int
      */
     int getFileCount();
     Catalogo();
+   ~Catalogo();
 };
 
+}
 #endif // CATALOGO_H
